e4bf3739d97ad5a3e1b3b0790e66a31942492386
ekwaro <dominicnoelchris@gmail.com>
2022-05-11 14:40:17 +0300

    Added a new village regiser project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        "coat": "url('./imges/logo.png')"
+      }
+    },
+  },
+  plugins: [],
+}
